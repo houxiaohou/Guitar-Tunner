c2e395f4e633796281182a8020ef7ff07140c093
Young90 <jackiehou@163.com>
2012-11-06 13:07:57 +0800

    Finish building the libFFt library

---
+++ obj/local/armeabi/objs/FFT/processRawData.o.d
@@ -0,0 +1,79 @@
+obj/local/armeabi/objs/FFT/processRawData.o: jni/processRawData.c \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/string.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/jni.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/math.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/limits.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/syslimits.h
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/string.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/math.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/limits.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/d/ndk/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
